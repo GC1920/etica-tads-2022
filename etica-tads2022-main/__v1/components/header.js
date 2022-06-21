15c1e8076302186aa6ab249e28d0b045a416f225
GC1920 <71650142+GC1920@users.noreply.github.com>
2022-06-20 21:48:36 -0400

    Add files via upload

---
+++ etica-tads2022-main/__v1/components/header.js
@@ -0,0 +1,10 @@
+const header = () => {
+    return `<header class="header">
+        <picture>
+            <source srcset="../assets/header-400.jpg" media="(max-width: 600px)">
+            <img src="../assets/header-1200.jpg">
+        </picture>
+    </header>`;
+}
+
+export default header;
